a451ff8ab0fd8f3e5a06a15069f020eb2b440d2a
Frank Matranga <thefrankmatranga@gmail.com>
2023-06-15 23:20:37 -0400

    Initial prototype

---
+++ src/App.tsx
@@ -0,0 +1,422 @@
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Avatar,
+  Box,
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardFooter,
+  Center,
+  Container,
+  Divider,
+  Flex,
+  HStack,
+  Heading,
+  Image,
+  SimpleGrid,
+  Skeleton,
+  Spacer,
+  Spinner,
+  Stack,
+  Tag,
+  Text,
+  Wrap,
+  WrapItem,
+  useColorMode,
+} from "@chakra-ui/react";
+
+import { PostgrestError, User, createClient } from "@supabase/supabase-js";
+import { Database } from "./database.types";
+import formatRelative from "date-fns/formatRelative";
+const supabaseUrl = "https://dmxnczlntlpsvwgutlml.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRteG5jemxudGxwc3Z3Z3V0bG1sIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY4MDMyMjUsImV4cCI6MjAwMjM3OTIyNX0.RBafDsI9C_QuV4ulNJ0ziqyg7cBwinmFr3jDWBPnc_o";
+const supabase = createClient<Database>(supabaseUrl, supabaseKey!);
+
+type Activity = Database["public"]["Tables"]["activities"]["Row"];
+type Event = Database["public"]["Tables"]["events"]["Row"];
+type RSVP = Database["public"]["Tables"]["rsvps"]["Row"];
+
+function formatMoney(cents: number) {
+  return `$${cents / 100}`;
+}
+
+function useLoading() {
+  return useState<boolean>(true);
+}
+
+type AppContext = {
+  user: User | null;
+  isUserLoading: boolean;
+
+  isEventsLoading: boolean;
+  eventsError: PostgrestError | null;
+  events: Event[];
+  activeEvent: Event | null;
+
+  isActivitiesLoading: boolean;
+  activitiesError: PostgrestError | null;
+  activities: Activity[];
+};
+const AppContext = createContext<AppContext>({
+  activities: [],
+  isActivitiesLoading: true,
+  activitiesError: null,
+  events: [],
+  activeEvent: null,
+  isEventsLoading: true,
+  eventsError: null,
+  user: null,
+  isUserLoading: true,
+});
+
+function useUser(): [boolean, User | null] {
+  const [isLoading, setIsLoading] = useLoading();
+  const [user, setUser] = useState<User | null>(null);
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setUser(session?.user ?? null);
+      setIsLoading(false);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  return [isLoading, user];
+}
+
+function useTable<T extends { id: any }>(
+  tableName: keyof Database["public"]["Tables"]
+): [boolean, PostgrestError | null, T[]] {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<PostgrestError | null>(null);
+  const [rows, setRows] = useState<T[]>([]);
+
+  useEffect(() => {
+    supabase
+      .from(tableName)
+      .select("*")
+      .then(({ data, error }) => {
+        if (error) {
+          console.error(error);
+          setError(error);
+        } else {
+          setError(null);
+        }
+
+        setIsLoading(false);
+        setRows((data as unknown as T[]) ?? []);
+      });
+
+    const channel = supabase
+      .channel("schema-db-changes")
+      .on(
+        "postgres_changes",
+        {
+          event: "UPDATE",
+          schema: "public",
+          table: tableName,
+        },
+        (payload) => {
+          setRows((rows) =>
+            rows.map((row) =>
+              row.id === payload.old.id ? (payload.new as T) : row
+            )
+          );
+        }
+      )
+      .on(
+        "postgres_changes",
+        {
+          event: "INSERT",
+          schema: "public",
+          table: tableName,
+        },
+        (payload) => setRows((rows) => [...rows, payload.new as T])
+      )
+      .on(
+        "postgres_changes",
+        {
+          event: "DELETE",
+          schema: "public",
+          table: tableName,
+        },
+        (payload) =>
+          setRows((rows) => rows.filter((row) => row.id !== payload.old.id))
+      )
+      .subscribe();
+
+    return () => {
+      channel
+        .unsubscribe()
+        .then((val) =>
+          console.log(`Unsubbed from table ${tableName} with status ${val}`)
+        );
+    };
+  }, []);
+
+  return [isLoading, error, rows];
+}
+
+/** Bar displaying logged in user with options to sign in/sign out or edit profile. */
+function UserProfile() {
+  const [isUserLoading, user] = useUser();
+
+  const discordIdentityData = user?.identities?.at(0)?.identity_data;
+
+  async function signInWithDiscord() {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "discord",
+    });
+    if (error) {
+      alert(
+        "There was an error signing in with Discord... Please try again later."
+      );
+    }
+  }
+
+  if (user) {
+    return (
+      <nav>
+        <Center>
+          <HStack spacing={3}>
+            <Skeleton isLoaded={!isUserLoading}>
+              <Avatar
+                bgColor={"gray"}
+                size={"sm"}
+                name={discordIdentityData?.full_name}
+                src={discordIdentityData?.avatar_url}
+              />
+            </Skeleton>
+            <Skeleton isLoaded={!isUserLoading}>
+              <Text fontSize={"3xl"}>
+                Hi, {discordIdentityData?.full_name ?? "User"}
+              </Text>
+            </Skeleton>
+            <Skeleton isLoaded={!isUserLoading}>
+              <Flex direction={"row"} gap={2} justifyItems={"center"}>
+                <Button size={"sm"}>Edit profile</Button>
+                <Button size={"sm"} onClick={() => supabase.auth.signOut()}>
+                  Log out
+                </Button>
+              </Flex>
+            </Skeleton>
+          </HStack>
+        </Center>
+      </nav>
+    );
+  } else {
+    return (
+      <Button onClick={() => signInWithDiscord()}>Login with Discord</Button>
+    );
+  }
+}
+
+function ActivitySkeleton() {
+  return (
+    <Card>
+      <CardBody>
+        <Stack spacing={3}>
+          <Skeleton height="150px" />
+          <Skeleton height={"20px"} />
+          <Skeleton height={"20px"} />
+          <Skeleton height={"20px"} />
+        </Stack>
+      </CardBody>
+    </Card>
+  );
+}
+
+/** Displays the details of an activity along with actions to vote and favorite. */
+function ActivityCard({ activity }: { activity: Activity }) {
+  return (
+    <Card>
+      <CardBody>
+        <Image
+          borderRadius={"lg"}
+          alt={activity.name}
+          src="https://th.bing.com/th/id/OIP.vaWPHjLHKA1TgRqH1IfvoQHaEK?pid=ImgDet&rs=1"
+        />
+        <Stack mt={6} spacing={3}>
+          <Heading size="lg">{activity.name}</Heading>
+          <Text>{activity.summary}</Text>
+          {activity.price ? (
+            <Text color="blue.600" fontSize="xl">
+              <strong>{formatMoney(activity.price)}</strong>{" "}
+              {activity.price_type === "once"
+                ? "one-time purchase"
+                : "subscription"}
+            </Text>
+          ) : (
+            <Text color="blue.600" fontSize="xl">
+              <strong>Free!!!</strong>
+            </Text>
+          )}
+          <Wrap>
+            {activity.tags.map((tag) => (
+              <WrapItem key={tag}>
+                <Tag>{tag}</Tag>
+              </WrapItem>
+            ))}
+          </Wrap>
+        </Stack>
+      </CardBody>
+      <Divider />
+      <CardFooter>
+        <ButtonGroup spacing={2}>
+          <Button variant={"solid"} colorScheme={"blue"}>
+            Vote to Play
+          </Button>
+          <Button variant="ghost" colorScheme="blue">
+            Favorite
+          </Button>
+        </ButtonGroup>
+      </CardFooter>
+    </Card>
+  );
+}
+
+function NoActiveEventView() {
+  const { events } = useContext(AppContext);
+
+  const nextEvent = useMemo(
+    () =>
+      events
+        .sort((a, b) => (a.start_at < b.start_at ? 1 : -1))
+        .find((ev) => new Date(ev.start_at) > new Date()),
+    [events]
+  );
+
+  return (
+    <Alert
+      status="info"
+      variant="subtle"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      textAlign="center"
+      height="200px"
+      my={10}
+    >
+      <AlertIcon boxSize="40px" mr={0} />
+      <AlertTitle mt={4} mb={1} fontSize="lg">
+        No game night tonight!
+      </AlertTitle>
+      <AlertDescription maxWidth="sm">
+        {nextEvent ? (
+          <Text>
+            The next one is scheduled for{" "}
+            <strong>
+              {formatRelative(new Date(nextEvent.start_at), new Date())}
+            </strong>
+          </Text>
+        ) : (
+          <Text>The next one is not yet scheduled.</Text>
+        )}
+      </AlertDescription>
+    </Alert>
+  );
+}
+
+function App() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  const [isActivitiesLoading, activitiesError, activities] =
+    useTable<Activity>("activities");
+  const [isEventsLoading, eventsError, events] = useTable<Event>("events");
+  const [isUserLoading, user] = useUser();
+
+  const activeEvent = useMemo<Event | null>(() => {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    return (
+      events.find((event) => {
+        const eventDay = new Date(event.start_at);
+        eventDay.setHours(0, 0, 0, 0);
+        return eventDay.getTime() === today.getTime();
+      }) ?? null
+    );
+  }, [events]);
+
+  const appData = useMemo(
+    () => ({
+      isActivitiesLoading,
+      activitiesError,
+      activities,
+      activeEvent,
+      isUserLoading,
+      user,
+      isEventsLoading,
+      events,
+      eventsError,
+    }),
+    [
+      isActivitiesLoading,
+      activitiesError,
+      activities,
+      activeEvent,
+      isUserLoading,
+      user,
+      isEventsLoading,
+      events,
+      eventsError,
+    ]
+  );
+
+  return (
+    <AppContext.Provider value={appData}>
+      <Container maxW="container.lg" my={10}>
+        <Flex minWidth={"max-content"} alignItems={"center"} gap={2} mb={10}>
+          <UserProfile />
+          <Spacer />
+          <Button size={"sm"} onClick={toggleColorMode}>
+            Toggle {colorMode === "light" ? "Dark" : "Light"}
+          </Button>
+        </Flex>
+
+        {!isUserLoading && user ? (
+          <main>
+            <Heading as="h1" size="4xl">
+              Rathskeller Game Night
+            </Heading>
+
+            {activeEvent ? null : <NoActiveEventView />}
+
+            <Heading>
+              {activeEvent
+                ? "What do you want to play tonight?"
+                : "Browse Activities"}
+            </Heading>
+            {activitiesError && (
+              <Alert status="error" my={"5"}>
+                <AlertIcon /> There was an error fetching the activities. Blame
+                Frank and try again later!
+              </Alert>
+            )}
+            <SimpleGrid columns={3} spacing={10} my={10}>
+              {isActivitiesLoading && !activities
+                ? [1, 2, 3].map((_, index) => <ActivitySkeleton key={index} />)
+                : activities!.map((activity) => (
+                    <ActivityCard key={activity.id} activity={activity} />
+                  ))}
+            </SimpleGrid>
+          </main>
+        ) : (
+          <Heading as={"p"} fontSize={"3xl"}>
+            Login with Discord to get started.
+          </Heading>
+        )}
+      </Container>
+    </AppContext.Provider>
+  );
+}
+
+export default App;
